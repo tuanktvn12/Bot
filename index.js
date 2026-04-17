1e4946f6439621b04b7034e888389ae01f4810c9
Lâm Ngọc tuấn <74889800+tuanktvn12@users.noreply.github.com>
2026-04-17 18:58:24 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+﻿const { spawn } = require("child_process");
+const logger = require(process.cwd() + "/utils/log.js");
+function startBot(message) {
+    (message) ? logger(message, "[ STARTING ]") : "";
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "Nika-betav3.js"], {
+        cwd: __dirname,
+        stdio: "inherit",
+        shell: true
+    });
+    child.on("close", (codeExit) => {
+        if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
+            startBot("Tiến Hành Khởi Động Lại...");
+            global.countRestart += 1;
+            return;
+        } else return;
+    });
+    child.on("error", function (error) {
+        logger("Đã xảy ra lỗi: " + JSON.stringify(error), "[ STARTING ]");
+    });
+}
+startBot();
